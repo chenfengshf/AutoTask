4ecc69e1db6f3770ae885718d01911b78f5e65d2
chenfengshf <chenfengshf@hotmail.com>
2021-10-11 13:58:58 +0800

    add jingcai

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 : for push to github
 git config --global https.proxy https://127.0.0.1:10809
 timeout /T 5 /NOBREAK
-git push --set-upstream origin main
+git push --force --set-upstream origin main
 git config --global --unset https.proxy
 git config --global -l
