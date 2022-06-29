218f6f6615674c6fa2c013f0a27d87c604af89e3
chenfengshf <chenfengshf@hotmail.com>
2022-06-29 15:16:36 +0800

    add kuaishou

---
+++ auto.js/test.js
@@ -0,0 +1,10 @@
+// console.log("1111");
+setScreenMetrics(1080, 1920);
+app.launch("com.sankuai.meituan")
+// click("提现")
+// var scope = text("Auto.js").findOne().bounds();
+toast("控件在屏幕上的范围为");
+// className("android.widget.LinearLayout").depth(1).findOne().click()
+swipe(300, 1000 , 600, 1000,1000)
+// alert("您好");
+// console.show(); 
